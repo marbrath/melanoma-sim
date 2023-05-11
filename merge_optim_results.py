315eb6bc639cfb476103e865df1498ee11a18754
Mari Brathovde <marbrath@uio.no>
2023-05-11 23:25:14 +0200

    merge_optim_results: Load hess inv

--- merge_optim_results.py
+++ merge_optim_results.py
@@ -9,6 +9,7 @@ if len(sys.argv) < 2:
 result_root = sys.argv[1]
 max_children = int(sys.argv[2]) if len(sys.argv) > 2 else None
 all_optim = []
+all_hess_inv = []
 
 result_suffix = f'_{max_children:02d}' if max_children is not None else None
 
@@ -21,8 +22,12 @@ for seedname in next(os.walk(result_root))[1]:
     try:
         res = np.load(os.path.join(result_path, 'optim.npy'))
         all_optim.append(res)
+
+        hess_inv = np.load(os.path.join(result_path, 'hessian_inv'))
+        all_hess_inv.append(hess_inv.diagonal())
     except:
         pass
 
 print(len(all_optim))
 np.save(os.path.join(result_root, 'all_optim.npy'), np.stack(all_optim))
+np.save(os.path.join(result_root, 'all_hess_inv.npy'), np.stack(all_hess_inv))
