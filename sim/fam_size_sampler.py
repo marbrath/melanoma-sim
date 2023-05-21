a4e0633fc46a95a4cf5dd103845ea954704c2238
Mari Brathovde <marbrath@uio.no>
2023-05-21 23:10:10 +0200

    Fixed num children sampling

--- sim/fam_size_sampler.py
+++ sim/fam_size_sampler.py
@@ -8,9 +8,9 @@ pdf = sizes_counts/sum(sizes_counts)
 cdf = np.cumsum(pdf)
 
 # see https://tmramalho.github.io/blog/2013/12/16/how-to-do-inverse-transformation-sampling-in-scipy-and-numpy/
-inv_cdf = scipy.interpolate.interp1d(cdf, sizes)
+inv_cdf = scipy.interpolate.interp1d(cdf, sizes, kind='next')
 
 def fam_size_sample(shape):
-	fam_size = inv_cdf(np.random.rand(*shape))
+	fam_size = inv_cdf(np.random.rand(*shape)).astype(int)
 	
 	return fam_size
